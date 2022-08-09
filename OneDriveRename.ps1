25e68f59396e7e8827fbf5af83a165e90fcb64fc
Jakub Kowalski <79029121+kuszy00@users.noreply.github.com>
2022-08-09 16:16:02 +0200

    Added StatusChecker and RenameFiles scripts
    Added OneDrive StatusChecker and RenameFiles scripts

---
+++ OneDriveRename.ps1
@@ -0,0 +1,22 @@
+﻿# Import: Import-Module C:\Temp\OneDriveCheck.ps1
+# Check: OneDriveCheck -folder <path>
+# Fix: OneDriveCheck -folder <path> -fix
+
+function OneDriveCheck($Folder,[switch]$Fix) {
+    $items = Get-ChildItem -Path $Folder -Recurse
+
+    $UnsupportedChars = "[^a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ ()@_=+.,';[\]\-]"
+
+    ForEach ( $item in $items ) {
+        if ( $item.Name -match $UnsupportedChars ) {
+            $newFileName = $item.Name
+            Write-Host "$($item.FullName) has invalid characters!" -ForegroundColor Red
+
+            if ( $Fix ) {
+                $newFileName = (( $item.PSChildName ) -creplace $UnsupportedChars,"" )
+                Rename-Item $item.FullName -newname ($newFileName)            
+                Write-Host "$($item.Name) has been changed to $newFileName" -ForegroundColor Green
+            }           
+        }
+    }
+}
---
+++ OneDriveStatus.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module C:\Temp\OneDriveLib.dll
+Get-ODStatus -Type Business1 | Out-File -FilePath c:\temp\StatusOD.txt
+
+$name = Get-Content c:\temp\StatusOD.txt | Select -First 5 | Select -Last 1
+$name.split([Environment]::NewLine) | ForEach-Object {
+    $userName = $_.Split(":")[1]
+    $userName = $userName.Split("\")[1]
+}
+
+$status = Get-Content c:\temp\StatusOD.txt | Select -First 8 | Select -Last 1
+$status.split([Environment]::NewLine) | ForEach-Object {
+    $userStatus = $_.Split(":")[1]
+}
+
+$userName + ';' + $userStatus | Out-File -FilePath "c:\temp\StatusOneDrive\$($userName)_$(Get-Date -Format "yyyyMMdd_HHmmss").txt"
+
+Remove-Item -Path c:\temp\StatusOD.txt
